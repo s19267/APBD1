474b6acebabf3bebe778408d06a461c29e1741ef
Maciej Petrykowski <s19267@pjwstk.edu.pl>
2020-03-05 12:56:36 +0100

    Pierwszy commit

---
+++ cw1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace cw1
+{
+    class Program
+    {
+        public static async Task  Main(string[] args)
+        {
+            String url = args[0];
+            var client =new HttpClient();
+            var page =await client.GetAsync(url);
+
+            if (!page.IsSuccessStatusCode) return;
+
+            String pageString = await page.Content.ReadAsStringAsync();
+            Regex reg = new Regex(@"\b\w*[0-9]*@\b[\w,\.]*");
+            MatchCollection mc= reg.Matches(pageString);
+
+           
+            
+            foreach(Match math in mc)
+            {
+                Console.WriteLine(math);
+            }
+
+
+        }
+    }
+}
